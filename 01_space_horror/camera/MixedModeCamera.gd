ed129a21433320febc30445247f66b18dcbe7b02
Kevin <kevinhobbs@gmail.com>
2020-05-25 17:05:13 -0500

    Starbeast approach/attack animations + screen shake

--- 01_space_horror/camera/MixedModeCamera.gd
+++ 01_space_horror/camera/MixedModeCamera.gd
@@ -5,6 +5,11 @@ class_name MixedModeCamera
 var target:Node2D
 export var targetPath:NodePath
 export var following:bool = false
+export(float) var shakeIntensity = 30
+export(float) var shakeDurationSeconds = 1.5
+var remainingShakeDurationSeconds = 0
+
+var shake:float = 0
 
 var lastEnteredRegion
 
@@ -14,6 +19,9 @@ func _ready():
 func _physics_process(_delta):
 	if following:
 		position = target.position
+	if remainingShakeDurationSeconds > 0:
+		position += Vector2(rand_range(-shakeIntensity, shakeIntensity), rand_range(-shakeIntensity, shakeIntensity))
+		remainingShakeDurationSeconds -= _delta
 
 func on_fixed_mode_entered(fixedArea:Position2D):
 	lastEnteredRegion = fixedArea
@@ -37,3 +45,7 @@ func transitionToNextRegion():
 	yield($AnimationPlayer, "animation_finished")
 	get_tree().paused = false
 	self.pause_mode = Node.PAUSE_MODE_INHERIT
+
+
+func _on_StarbeastRoom_approachFinished():
+	remainingShakeDurationSeconds = shakeDurationSeconds
--- 01_space_horror/levels/starbeast/StarbeastRoom.gd
+++ 01_space_horror/levels/starbeast/StarbeastRoom.gd
@@ -6,14 +6,21 @@ signal attackFinished
 export(float) var approachMinutes = 1
 
 func _ready():
+	startApproach()
+
+func startApproach():
 	$AnimationPlayer.playback_speed = 1/approachMinutes
 	$AnimationPlayer.play("approach")
-
+	
+func startAttack():
+	$AnimationPlayer.playback_speed = 1
+	$AnimationPlayer.play("attack")
+	
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "approach":
 		emit_signal("approachFinished")
-		$AnimationPlayer.play("attack")
+		startAttack()
 	if anim_name == "attack":
 		emit_signal("attackFinished")
-		$AnimationPlayer.play("approach")
+		startApproach()
